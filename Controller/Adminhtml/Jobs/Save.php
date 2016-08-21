2b4a784f40a1b15aa6b7b8110903600df00f0a6f
Rakesh Mistry <rakesh@strakertranslations.com>
2016-08-22 10:06:54 +1200

    Start on job categories

--- Controller/Adminhtml/Jobs/Save.php
+++ Controller/Adminhtml/Jobs/Save.php
@@ -4,35 +4,15 @@ namespace Straker\EasyTranslationPlatform\Controller\Adminhtml\Jobs;
 
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
-use Magento\Eav\Model\AttributeRepository;
-use Magento\Store\Model\StoreManagerInterface;
-use Straker\EasyTranslationPlatform\Helper\ConfigHelper;
-use Straker\EasyTranslationPlatform\Helper\XmlHelper;
-use Straker\EasyTranslationPlatform\Model\JobType;
-use Straker\EasyTranslationPlatform\Model\JobRepository;
 use Straker\EasyTranslationPlatform\Helper\JobHelper;
 use Straker\EasyTranslationPlatform\Api\Data\StrakerAPIInterface;
 use Straker\EasyTranslationPlatform\Api\Data\SetupInterface;
 use Straker\EasyTranslationPlatform\Logger\Logger;
 
-class Save extends \Magento\Backend\App\Action
+class Save extends Action
 {
-    /**
-     * @var \Magento\Backend\Helper\Js
-     */
-    protected $_jsHelper;
 
     protected $_setupInterface;
-
-    /**
-     * @var \Straker\EasyTranslationPlatform\Helper\ConfigHelper
-     */
-    protected $_configHelper;
-
-    /**
-     * @var \Straker\EasyTranslationPlatform\Model\ResourceModel\Job\CollectionFactory
-     */
-    protected $_jobCollectionFactory;
 
 
     protected $_multiSelectInputTypes = array(
@@ -43,40 +23,25 @@ class Save extends \Magento\Backend\App\Action
         'magento_destination_store','straker_target_language','magento_source_store','straker_source_language'
     );
 
-    protected  $_translatedAttributeLabels = [];
-
-    protected  $_translatedAttributeOptions = [];
 
     protected $_jobRequest;
 
-
     /**
-     * \Magento\Backend\Helper\Js $jsHelper
-     * @param Action\Context $context
+     * Save constructor.
+     * @param Context $context
+     * @param JobHelper $jobHelper
+     * @param StrakerAPIInterface $API
+     * @param SetupInterface $setup
+     * @param Logger $logger
      */
     public function __construct(
         Context $context,
-        ConfigHelper $configHelper,
-        \Magento\Backend\Helper\Js $jsHelper,
-        AttributeRepository $attributeRepository,
-        StoreManagerInterface $storeManager,
-        XmlHelper $xmlHelper,
-        JobRepository $jobRepository,
-        JobType $jobType,
         JobHelper $jobHelper,
         StrakerAPIInterface $API,
         SetupInterface $setup,
-        Logger $logger,
-        \Straker\EasyTranslationPlatform\Model\ResourceModel\Job\CollectionFactory $jobCollectionFactory
+        Logger $logger
     ) {
-        $this->_configHelper = $configHelper;
-        $this->_jsHelper = $jsHelper;
-        $this->_jobCollectionFactory = $jobCollectionFactory;
-        $this->_attributeRepository = $attributeRepository;
-        $this->_xmlHelper = $xmlHelper;
-        $this->_storeManager = $storeManager;
-        $this->_jobTypeModel = $jobType;
-        $this->jobRepository = $jobRepository;
+
         $this->_api = $API;
         $this->_setupInterface = $setup;
         $this->_jobHelper = $jobHelper;
@@ -102,7 +67,8 @@ class Save extends \Magento\Backend\App\Action
     {
         $data = $this->getRequest()->getPostValue();
 
-        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        var_dump($data);
+        exit;
 
         $resultRedirect = $this->resultRedirectFactory->create();
 
@@ -136,8 +102,6 @@ class Save extends \Magento\Backend\App\Action
                 $this->_logger->error('error'.__FILE__.' '.__LINE__,array($e));
 
             } catch (\Exception $e) {
-
-//                var_dump($e->getMessage());
 
                 $this->messageManager->addException($e, __('Something went wrong while saving the job.'.$e->getMessage()));
 
@@ -202,7 +166,6 @@ class Save extends \Magento\Backend\App\Action
         $this->_jobRequest['source_file'] = $job_object->getData('source_file');
         $this->_jobRequest['token']       = $job_object->getId();
 
-//        var_dump($this->_jobRequest);exit();
         $response = $this->_api->callTranslate($this->_jobRequest);
 
         try {
