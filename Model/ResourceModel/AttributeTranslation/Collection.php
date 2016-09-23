46aca4eaafe8cf0ff91880d2d495afd07b3da1a8
Paul Dong <paul.d@strakertranslations.com>
2016-09-23 17:18:27 +1200

    fix category name issue

--- Model/ResourceModel/AttributeTranslation/Collection.php
+++ Model/ResourceModel/AttributeTranslation/Collection.php
@@ -18,14 +18,14 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
         return $this;
     }
 
-    function addCategoryName( $sourceStoreId = 0 ){
+    function addCategoryName( $sourceStoreId = 0, $attrId = 0 ){
         $categoryTable = $this->getTable('catalog_category_entity_varchar');
 
         if( $sourceStoreId == 0 ){
             $this->getSelect()
                 ->joinLeft(
                     ['cn'=> $categoryTable],
-                    'main_table.entity_id = cn.entity_id AND cn.store_id = 0 AND cn.attribute_id = 42',
+                    'main_table.entity_id = cn.entity_id AND cn.store_id = 0 AND cn.attribute_id = ' . $attrId,
                     ['name' => 'value']
                 );
         }else{
@@ -34,11 +34,11 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
                     'if(cn_store.value IS NOT NULL, cn_store.value, cn_default.value) AS name'
                 )->joinLeft(
                     ['cn_store'=> $categoryTable],
-                    'main_table.entity_id = cn_store.entity_id AND cn_store.store_id = ' .$sourceStoreId . ' AND cn_store.attribute_id = 42',
+                    'main_table.entity_id = cn_store.entity_id AND cn_store.store_id = ' .$sourceStoreId . ' AND cn_store.attribute_id = ' . $attrId,
                     []
                 )->joinLeft(
                     ['cn_default'=> $categoryTable],
-                    'main_table.entity_id = cn_default.entity_id AND cn_default.store_id = 0 AND cn_default.attribute_id = 42',
+                    'main_table.entity_id = cn_default.entity_id AND cn_default.store_id = 0 AND cn_default.attribute_id = ' . $attrId,
                     []
                 );
         }
