63db4bad7518bd554b8a324cd477dee826f4eb0f
Paul Dong <paul.d@strakertranslations.com>
2016-10-06 13:27:37 +1300

    IMA-84: change filter type from text to select for languages

---
+++ Model/Config/Source/SourceLanguage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Straker\EasyTranslationPlatform\Model\Config\Source;
+
+class SourceLanguage implements \Magento\Framework\Option\ArrayInterface
+{
+    protected $_jobCollectionFactory;
+
+    function __construct(
+        \Straker\EasyTranslationPlatform\Model\ResourceModel\Job\CollectionFactory $jobCollectionFactory
+    )
+    {
+        $this->_jobCollectionFactory = $jobCollectionFactory;
+    }
+
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        /** @var \Straker\EasyTranslationPlatform\Model\ResourceModel\Job\Collection $collection */
+        $collection = $this->_jobCollectionFactory->create();
+        $collection->distinct(true)->addFieldToSelect('sl');
+        $languages = [];
+        foreach ( $collection->getItems() as $job ){
+            array_push($languages, ['value' => $job->getSl(), 'label'=> $job->getSl()]);
+        }
+        return $languages;
+    }
+}
