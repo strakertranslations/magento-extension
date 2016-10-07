a3a47e31f18b5572d62b6d7bf578ae06688edde5
Jiro Sasamoto <jiro@strakertranslations.com>
2016-10-07 15:35:37 +1300

    git reset

--- app/code/community/StrakerTranslations/EasyTranslationPlatform/Block/Adminhtml/New/Category/Attribute.php
+++
@@ -1,6 +0,0 @@
-<?php
-class StrakerTranslations_EasyTranslationPlatform_Block_Adminhtml_New_Category_Attribute extends Mage_Adminhtml_Block_Widget_Container{
-    protected function _beforeToHtml(){
-        $this->setTemplate('straker/new/category/attributes.phtml');
-    }
-}
