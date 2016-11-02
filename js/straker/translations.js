62caef9ca2f13ce51ebadfbeab0df105977632dc
Paul Dong <paul.d@strakertranslations.com>
2016-11-02 14:54:48 +1300

    IMA-138: instead of retrieve elements base on url, change to find text in straker menu

--- js/straker/translations.js
+++ js/straker/translations.js
@@ -1,16 +1,19 @@
 Event.observe(window, "load", function() {
-    var strakerMenu = $$("#nav LI.level0:has(A[href*=admin/straker])")[0];
+    // var strakerMenu = $$('#nav LI.level0:has(A[href*=admin/straker])')[0];
+    var strakerMenu = $$('#nav LI.level0:has(SPAN:contains("Straker"))')[0];
     var n = SKIN_URL.indexOf("adminhtml");
-    var imageElement = '<img style="height: 23px;margin-right: 3px;line-height: 27px;vertical-align: middle;margin-top: -2px;" id="straker_block" src="'+SKIN_URL.substring(0, n)+'adminhtml/default/straker/images/straker-translations-logo.png"." alt="" border="0" />';
+    var imageElement = '<img style="height: 23px;margin-right: 3px;line-height: 27px;vertical-align: middle;margin-top: -2px;" id="straker_block" src="'+SKIN_URL.substring(0, n)+'adminhtml/default/straker/images/straker-translations-logo.png" alt="" border="0" />';
     $(strakerMenu.select(' > a > span'))[0].setStyle({display: 'inline-block'}).insert({before: imageElement});
 
-    var strakerAccountLink = $$('a[href*="admin/straker_new/account"]')[0];
+    // var strakerAccountLink = $$('a[href*="admin/straker_new/account"]')[0];
+    var strakerAccountLink = strakerMenu.select('A:has(span:contains("Account"))')[0];
     strakerAccountLink.observe('click', function(event) {
         window.open(strakerAccountLink.href, '_blank');
         Event.stop(event);
     });
 
-    var strakerTermsLink = $$('a[href*="admin/straker_new/terms"]')[0];
+    // var strakerTermsLink = $$('a[href*="admin/straker_new/terms"]')[0];
+    var strakerTermsLink = strakerMenu.select('A:has(span:contains("Terms"))')[0];
     strakerTermsLink.observe('click', function(event) {
         window.open(strakerTermsLink.href, '_blank');
         Event.stop(event);
