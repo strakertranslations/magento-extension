2eb213c5940036bc20119121b674abb108f3c00e
Paul Dong <paul.d@strakertranslations.com>
2016-11-03 08:15:30 +1300

    IMA-138: remove 'admin' from selector

--- js/straker/translations.js
+++ js/straker/translations.js
@@ -1,16 +1,20 @@
 Event.observe(window, "load", function() {
-    var strakerMenu = $$("#nav LI.level0:has(A[href*=admin/straker])")[0];
+    // var strakerMenu = $$("#nav LI.level0:has(A[href*=admin/straker])")[0];
+    var strakerMenu = $$("#nav LI.level0:has(A[href*=/straker_new])")[0];
     var n = SKIN_URL.indexOf("adminhtml");
     var imageElement = '<img style="height: 23px;margin-right: 3px;line-height: 27px;vertical-align: middle;margin-top: -2px;" id="straker_block" src="'+SKIN_URL.substring(0, n)+'adminhtml/default/straker/images/straker-translations-logo.png"." alt="" border="0" />';
     $(strakerMenu.select(' > a > span'))[0].setStyle({display: 'inline-block'}).insert({before: imageElement});
 
-    var strakerAccountLink = $$('a[href*="admin/straker_new/account"]')[0];
+    // var strakerAccountLink = $$('a[href*="admin/straker_new/account"]')[0];
+    var strakerAccountLink = strakerMenu.select('a[href*="/straker_new/account"]')[0];
+
     strakerAccountLink.observe('click', function(event) {
         window.open(strakerAccountLink.href, '_blank');
         Event.stop(event);
     });
 
-    var strakerTermsLink = $$('a[href*="admin/straker_new/terms"]')[0];
+    // var strakerTermsLink = $$('a[href*="admin/straker_new/terms"]')[0];
+    var strakerTermsLink = $$('a[href*="/straker_new/terms"]')[0];
     strakerTermsLink.observe('click', function(event) {
         window.open(strakerTermsLink.href, '_blank');
         Event.stop(event);
