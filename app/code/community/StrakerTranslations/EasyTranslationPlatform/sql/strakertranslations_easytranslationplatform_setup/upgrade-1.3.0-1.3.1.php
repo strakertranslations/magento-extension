287a513c495198c3bd135b724e667c928eb8b90a
Paul Dong <paul.d@strakertranslations.com>
2016-11-14 15:58:34 +1300

    fix incorrect foreign keys

--- app/code/community/StrakerTranslations/EasyTranslationPlatform/sql/strakertranslations_easytranslationplatform_setup/upgrade-1.3.0-1.3.1.php
+++ app/code/community/StrakerTranslations/EasyTranslationPlatform/sql/strakertranslations_easytranslationplatform_setup/upgrade-1.3.0-1.3.1.php
@@ -30,3 +30,64 @@ if ($connection->isTableExists($tableName) == true) {
             'new_entity_id'
         );
 }
+
+$tables = [
+    'straker_actionlog',
+    'st_straker_cms_block_attributes',
+    'st_straker_cms_block_translate',
+    'st_straker_cms_page_attributes',
+    'st_straker_cms_page_translate',
+    'st_straker_job_cmsblock',
+    'st_straker_job_cmspage'
+];
+
+$prefix = Mage::getConfig()->getTablePrefix();
+
+
+//format of returned foreign key
+//[
+//    'FK_NAME',
+//    'SCHEMA_NAME',
+//    'TABLE_NAME',
+//    'COLUMN_NAME',
+//    'REF_SHEMA_NAME',
+//    'REF_TABLE_NAME',
+//    'REF_COLUMN_NAME',
+//    'ON_DELETE',
+//    'ON_UPDATE'
+//];
+
+foreach ($tables as $table){
+    $tableName = $installer->getTable($table);
+    $fks = $connection->getForeignKeys($tableName);
+    if(strcasecmp($table, 'straker_actionlog') === 0){
+        $fk = reset($fks);
+        $connection->dropForeignKey($tableName, $fk['FK_NAME']);
+        $connection->addForeignKey(
+            $installer->getFkName($tableName, 'user_id', 'admin_user', 'user_id'),
+            $tableName,
+            'user_id',
+            $installer->getTable('admin_user'),
+            'user_id',
+            Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE
+        );
+    }else{
+        foreach ($fks as $fk){
+            if(strcasecmp($installer->getTable($fk['REF_TABLE_NAME']), $fk['REF_TABLE_NAME']) !== 0){
+                $connection->dropForeignKey($tableName, $fk['FK_NAME']);
+                $connection->addForeignKey(
+                    $installer->getFkName($tableName, $fk['COLUMN_NAME'], $installer->getTable($fk['REF_TABLE_NAME']), $fk['REF_COLUMN_NAME']),
+                    $tableName,
+                    $fk['COLUMN_NAME'],
+                    $installer->getTable($fk['REF_TABLE_NAME']),
+                    $fk['REF_COLUMN_NAME'],
+                    Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE
+                );
+            }
+        }
+    }
+
+}
+
+
+
